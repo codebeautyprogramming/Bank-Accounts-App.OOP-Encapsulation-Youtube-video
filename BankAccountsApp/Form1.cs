014a691647cba6007402e352b66c44dcb4d7b8f5
codebeautyprogramming <codebeautyprogramming@gmail.com>
2024-09-12 23:24:09 +0200

    Startup code for this chapter

---
+++ BankAccountsApp/Form1.cs
@@ -0,0 +1,29 @@
+namespace BankAccountsApp
+{
+    public partial class Form1 : Form
+    {
+        List<BankAccount> BankAccounts = new List<BankAccount>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateAccountBtn_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(OwnerTxt.Text))
+                return;
+
+            BankAccount bankAccount = new BankAccount(OwnerTxt.Text);
+            BankAccounts.Add(bankAccount);
+
+            RefreshGrid();
+            OwnerTxt.Text = string.Empty;
+        }
+
+        private void RefreshGrid()
+        {
+            BankAccountsGrid.DataSource = null;
+            BankAccountsGrid.DataSource = BankAccounts;
+        }
+    }
+}
